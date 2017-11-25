5d6e9f538b0d587e4d8736f5a1033df763e94923
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-26 11:34:51 +1300

    Fixed image paths

--- scripts/corpse/event.js
+++ scripts/corpse/event.js
@@ -13,7 +13,7 @@ function Event(date,data)
   this.posi = data.posi;
 
   this.el.innerHTML = ""
-  this.el.innerHTML += this.type == "video" ? `<a href='`+this.link+`' target='_blank'><media style='background-image:url(media/content/event.`+(this.name ? this.name.replace(/ /g,'.') : '')+`.jpg)'></media></a>` : ''
+  this.el.innerHTML += this.type == "video" ? `<a href='`+this.link+`' target='_blank'><media style='background-image:url(media/content/event.`+(this.name ? this.name.replace(/ /g,'.').toLowerCase() : '')+`.jpg)'></media></a>` : ''
   this.el.innerHTML += "<text><span class='title'>"+this.name+"</span><span class='details'>"+(this.text ? this.text : (this.vlue ? this.vlue : ''))+"</span></text>"
   this.el.innerHTML += `<svg class="icon"><circle cx="10.5" cy="10.5" r="3"></circle></svg><line class="spacer"></line>`
 }
