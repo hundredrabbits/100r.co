bb43b8af4f049396479aa3de781b70519ce044ce
Devine Lu Linvega <aliceffekt@gmail.com>
2017-12-10 18:00:02 +1300

    Fixed links

--- scripts/corpse/event.js
+++ scripts/corpse/event.js
@@ -14,7 +14,7 @@ function Event(date,data)
 
   this.el.innerHTML = ""
   this.el.innerHTML += this.type == "video" || this.type == "release" ? `<a href='`+this.link+`' target='_blank'><media style='background-image:url(media/content/event.`+(this.name ? this.name.replace(/ /g,'.').toLowerCase() : '')+`.jpg)'></media></a>` : ''
-  this.el.innerHTML += "<text><span class='title'>"+this.name+"</span><span class='details'>"+(this.text ? this.text : (this.vlue ? this.vlue : ''))+"</span></text>"
+  this.el.innerHTML += "<text><span class='title'>"+(this.link ? "<a href='"+this.link+"'>"+this.name+"</a>" : this.name)+"</span><span class='details'>"+(this.text ? this.text : (this.vlue ? this.vlue : ''))+"</span></text>"
   this.el.innerHTML += `<svg class="icon"><circle cx="10.5" cy="10.5" r="3"></circle></svg><line class="spacer"></line>`
 }
 
