a398f0641b5011381457754de6ab406afc66f1e9
Devine Lu Linvega <aliceffekt@gmail.com>
2019-08-21 14:37:58 -0700

    *

--- build/feed.js
+++ build/feed.js
@@ -26,7 +26,7 @@ function Feed (id, table) {
       if (id === 'SETTINGS') { continue }
       parts = parts.concat(entry[id])
     }
-    const description = runic(parts).replaceAll('../media/blog/','https://100r.co/media/blog/').toRss()
+    const description = runic(parts).replaceAll('../media/blog/', 'https://100r.co/media/blog/').toRss()
 
     return {
       title: id.toCapitalCase(),
@@ -35,7 +35,7 @@ function Feed (id, table) {
     }
   }
 
-  function _item (entry,id) {
+  function _item (entry, id) {
     return `
   <item>
     <title>${entry.title}</title>
@@ -59,7 +59,7 @@ function Feed (id, table) {
   <link><![CDATA[https://100r.co/blog]]></link>
   <description>The Rabbits' Journal</description>
   <generator>Oscean - Riven</generator>
-  ${parse(table).reduce((acc, val, key) => { return `${acc}${_item(val,key)}` }, '')}
+  ${parse(table).reduce((acc, val, key) => { return `${acc}${_item(val, key)}` }, '')}
 </channel>
 </rss>`.trim()
   }
@@ -69,16 +69,16 @@ String.prototype.toRss = function () {
   return this.replace(/&(?!\w*;)/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\"/g, '&quot;').replace(/\'/g, '&apos;')
 }
 
-String.prototype.toEntities = function () { 
-  return this.replace(/[\u00A0-\u9999<>\&]/gim, function (i) { return `&#${i.charCodeAt(0)}` }) 
+String.prototype.toEntities = function () {
+  return this.replace(/[\u00A0-\u9999<>\&]/gim, function (i) { return `&#${i.charCodeAt(0)}` })
 }
 
 String.prototype.toPath = function () {
   return this.toLowerCase().replace(/\+/g, '.').replace(/ /g, '.').replace(/[^0-9a-z\.\-\/]/gi, '').trim()
 }
 
-String.prototype.replaceAll = function (search, replacement) { 
-  return `${this}`.split(search).join(replacement) 
+String.prototype.replaceAll = function (search, replacement) {
+  return `${this}`.split(search).join(replacement)
 }
 
 module.exports = Feed
