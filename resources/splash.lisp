41095c5a52c3a2ad0e43295984d54d6a00569c6f
Devine Lu Linvega <aliceffekt@gmail.com>
2019-09-13 13:06:45 -0400

    Updated Ronin file

--- resources/splash.lisp
+++ resources/splash.lisp
@@ -1,20 +1,18 @@
 ; template for the hundred rabbits videos end screen
 (clear)
 (resize 1280 800)
-(def frame-rect 
-  (frame))
 (def import-path $path)
 (def export-path 
   (concat 
     (dirpath import-path) "/" 
     (filename import-path) "-export.jpg"))
-(fill frame-rect "white")
+(fill frame "white")
 (import import-path 
   (guide 
-    (circle frame-rect:c frame-rect:m 375)))
+    (circle frame:c frame:m 375)))
 (fill 
   (text 
-    (frame-rect:c) 
-    (mul frame-rect:m 1.6) "bold 45" "Support us on Patreon" "center" "Alte Haas Grotesk") "black")
+    (frame:c) 
+    (mul frame:m 1.6) "bold 45" "Support us on Patreon" "center" "Alte Haas Grotesk") "black")
 (export export-path)
 (exit true)
