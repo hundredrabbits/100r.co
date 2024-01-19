eec7d13e1d4fccdb987b10ed7f62e2c19e685b19
rekkabell <rekkabell@gmail.com>
2024-01-19 09:41:50 -0800

    *

--- src/main.c
+++ src/main.c
@@ -203,7 +203,7 @@ build(FILE *f, Lexicon *l, char *name, char *srcpath)
 	/* footer */
 	fputs("<footer><hr />", f);
 	fpedited(f, srcpath);
-	fputs("<b>Hundredrabbits</b> © 2023 — ", f);
+	fputs("<b>Hundredrabbits</b> © 2024 — ", f);
 	fputs("<a href='" LICENSE "' target='_blank'>BY-NC-SA 4.0</a>", f);
 	fputs("</footer>", f);
 	/* end */
